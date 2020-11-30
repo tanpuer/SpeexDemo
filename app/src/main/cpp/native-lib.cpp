850ae9979af7f4a82366685e1a1b73caf9a23de4
陈伟 <templechen>
2020-11-30 16:22:54 +0800

    rename

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_yupaopao_speedxdemo_MainActivity_stringFromJNI(
+Java_com_example_speexdemo_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
--- app/src/main/java/com/example/speexdemo/MainActivity.kt
+++ app/src/main/java/com/example/speexdemo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.yupaopao.speedxdemo
+package com.example.speexdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
