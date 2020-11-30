7c973e0b73ed8e99722736d39d21eba8123db2c1
陈伟 <templechen>
2020-11-30 20:31:21 +0800

    简单降噪/自增益/回声消除

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,89 @@
 #include <jni.h>
 #include <string>
+#include "speex_preprocess.h"
+#include "native_log.h"
+#include "vector"
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_speexdemo_MainActivity_stringFromJNI(
+SpeexPreprocessState *st = nullptr;
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_example_speexdemo_MainActivity_nativeInitSpeex(
         JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        jobject thiz,
+        jint frameSize,
+        jint sampleRate) {
+    st = speex_preprocess_state_init(frameSize, sampleRate);
+    int param = 1;
+    int result = 0;
+    result = speex_preprocess_ctl(st, SPEEX_PREPROCESS_SET_DENOISE, &param);
+    if (result != 0) {
+        ALOGD("deNoise init error %d", result)
+        return;
+    }
+    param = -25;
+    result = speex_preprocess_ctl(st, SPEEX_PREPROCESS_SET_NOISE_SUPPRESS, &param);
+    if (result != 0) {
+        ALOGD("set noise suppress error %d", result)
+        return;
+    }
+    param = 1;
+    result = speex_preprocess_ctl(st, SPEEX_PREPROCESS_SET_AGC, &param);
+    if (result != 0) {
+        ALOGD("set agc error %d", result)
+        return;
+    }
+    float level = 8000.0f;
+    result = speex_preprocess_ctl(st, SPEEX_PREPROCESS_SET_AGC_LEVEL, &level);
+    if (result != 0) {
+        ALOGD("set agc level error %d", param)
+        return;
+    }
+    param = 0;
+    result = speex_preprocess_ctl(st, SPEEX_PREPROCESS_SET_DEREVERB, &param);
+    if (result != 0) {
+        ALOGD("set dereverb error %d", result);
+        return;
+    }
+    float f = .0f;
+    result = speex_preprocess_ctl(st, SPEEX_PREPROCESS_SET_DEREVERB_DECAY, &f);
+    if (result != 0) {
+        ALOGD("set dereverb decay error %d", result);
+        return;
+    }
+    f = .0f;
+    result = speex_preprocess_ctl(st, SPEEX_PREPROCESS_SET_DEREVERB_LEVEL, &f);
+    if (result != 0) {
+        ALOGD("set dereverb level error %d", result);
+        return;
+    }
+    ALOGD("speex init success")
+}
+
+extern "C" JNIEXPORT jshortArray JNICALL
+Java_com_example_speexdemo_MainActivity_nativeSpeexProcessFrame(
+        JNIEnv *env,
+        jobject thiz,
+        jshortArray rawShortArray) {
+    if (st != nullptr) {
+        jsize size = env->GetArrayLength(rawShortArray);
+        std::vector<short> input(size);
+        env->GetShortArrayRegion(rawShortArray, 0, size, &input[0]);
+        int result = speex_preprocess_run(st, &input[0]);
+        ALOGD("speex process result %d", result)
+        jshortArray outShortArray = env->NewShortArray(input.size());
+        env->SetShortArrayRegion(outShortArray, 0, input.size(), &input[0]);
+        return outShortArray;
+    }
+    return rawShortArray;
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_example_speexdemo_MainActivity_nativeReleaseSpeex(
+        JNIEnv *env,
+        jobject thiz) {
+    if (st != nullptr) {
+        speex_preprocess_state_destroy(st);
+        st = nullptr;
+        ALOGD("release speex success")
+    }
 }
--- app/src/main/java/com/example/speexdemo/MainActivity.kt
+++ app/src/main/java/com/example/speexdemo/MainActivity.kt
@@ -1,23 +1,112 @@
 package com.example.speexdemo
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.*
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.IOException
+import java.nio.ByteBuffer
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAudioRecord: AudioRecord
+    private lateinit var mAudioTrack: AudioTrack
+    private var writeThread: Thread? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Example of a call to a native method
-        findViewById<TextView>(R.id.sample_text).text = stringFromJNI()
+        if (Build.VERSION.SDK_INT >= 21 && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                10001
+            )
+        } else {
+            initAudio()
+        }
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 10001 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            initAudio()
+        } else {
+            Toast.makeText(this, "must need audio record permission!", Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+    private fun initAudio() {
+        nativeInitSpeex(DEFAULT_BUFFER_SIZE, 44100)
+        initAudioTrack()
+        initAudioRecord()
+    }
+
+    private fun initAudioTrack() {
+        val minBufferSize = AudioTrack.getMinBufferSize(
+            44100,
+            AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT
+        )
+        mAudioTrack = AudioTrack(
+            AudioManager.STREAM_MUSIC,
+            44100,
+            AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT,
+            minBufferSize,
+            AudioTrack.MODE_STREAM
+        )
+    }
+
+    private fun initAudioRecord() {
+        val minBufferSize = AudioRecord.getMinBufferSize(44100, 1, AudioFormat.ENCODING_PCM_16BIT)
+        mAudioRecord = AudioRecord(
+            MediaRecorder.AudioSource.MIC,
+            44100,
+            1,
+            AudioFormat.ENCODING_PCM_16BIT,
+            minBufferSize
+        )
+        writeThread = object : Thread() {
+            override fun run() {
+                try {
+                    mAudioRecord.startRecording()
+                    val buffer = ShortArray(minBufferSize)
+                    while (mAudioRecord.recordingState == AudioRecord.RECORDSTATE_RECORDING) {
+                        mAudioRecord.read(buffer, 0, minBufferSize)
+
+                        val result = nativeSpeexProcessFrame(buffer)
+
+                        mAudioTrack.write(result, 0, result.size)
+                    }
+                    writeThread?.join()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        writeThread?.start()
+        mAudioTrack.play()
+    }
+
+    external fun nativeInitSpeex(frameSize: Int, sampleRate: Int)
+    external fun nativeSpeexProcessFrame(shortArray: ShortArray): ShortArray
+    external fun nativeReleaseSpeex()
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
